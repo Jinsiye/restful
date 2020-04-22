137d55308e8a21a97cb1fbcaa15c7c8d2fc6d0dc
tom_glb <goodMorning_glb@qq.com>
2020-04-22 10:00:55 +0800

    配置修改

--- src/main/java/com/king/restful/RestfulApplication.java
+++ src/main/java/com/king/restful/RestfulApplication.java
@@ -3,10 +3,13 @@ package com.king.restful;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Collections;
+
 @SpringBootApplication
 public class RestfulApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(RestfulApplication.class, args);
+        Collections
+
     }
 }
